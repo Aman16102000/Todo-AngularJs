e7bfe4b61f881d501a8efc808ff851e9d322882a
Aman Thakur <amanthakur5908@gmail.com>
2021-04-29 02:44:18 +0530

    Initial commit

---
+++ todo.js
@@ -0,0 +1,45 @@
+angular.module("TodoApp",[])
+.controller("TodoController",['$scope',function($scope){
+
+    //$scope is an object which has property as todolist which is
+    //also an object
+   // console.log($scope);
+  //  console.log($scope.todoList);
+
+
+    var todoList=$scope.todoList;
+
+    todoList.todos=[
+        {
+            task:"Learn angularJs",
+            done:false
+        },
+        {
+            task:"Running",
+            done:false
+        }
+    ]
+    angular.forEach(todoList.todos,function(todo)
+    {
+        console.log(todo.task+" "+todo.done);
+    })
+    todoList.add=function()
+    {
+        todoList.todos.push({task:todoList.text,done:false})
+        todoList.text="";
+        console.log(todoList);
+    }
+
+    todoList.delete=function()
+    {
+        var oldArray=todoList.todos;
+        todoList.todos=[];
+         angular.forEach(oldArray,function(todo)
+        {
+            if(todo.done!=true)
+            {
+                todoList.todos.push(todo);
+            }
+        })
+    }
+}])
